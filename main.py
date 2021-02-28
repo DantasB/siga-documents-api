ad90690cf16fe6873163c8f55b09d29cfb99d92b
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-02-28 14:05:20 -0300

    [API] Adding error treatment.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
-from flask import Flask, Response, jsonify
+from flask import Flask, jsonify
 from SharedLibrary.pdf_utils import *
 from SharedLibrary.siga_utils import *
+from Objects.response_object import *
 from Program.siga_core import *
 
 app = Flask(__name__)
@@ -14,8 +15,29 @@ def get_disponible_document_type():
 
 @app.route('/<login>/<password>/<document_type>')
 def get_document(login, password, document_type):
-    pass
+    response = Response()
 
+    if(treat_login(login) == ""):
+        response.error_message = "LOGIN IS NOT A VALID CPF. CHECK YOUR CREDENTIALS."
+        response.status        = False
+        return jsonify(response.serialize())
+    
+    if(document_type not in list_of_documents):
+        response.error_message = "DOCUMENT TYPE NOT VALID. CHECK YOUR CREDENTIALS."
+        response.status        = False
+        return jsonify(response.serialize())
+    
+    pdf_path, pdf = loop.run_until_complete(get_document_from_siga(login, password, document_type))
+    if(not is_valid_file_path(pdf_path)):
+        response.error_message = "PDF NOT VALID. CHECK IF YOU HAVE THIS DOCUMENT, YOUR CREDENTIALS ARE CORRECT OR IF SIGA IS NOT ONLINE."
+        response.status        = False
+        return jsonify(response.serialize())
+    
+    delete_document(pdf_path.split('/',2)[0])
+    
+    response.pdf_document = list(pdf)
+    response.status       = True
+    return jsonify(response.serialize())
 
 if __name__ == "__main__":
     app.run(debug=True)
