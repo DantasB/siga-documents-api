e755d5684f6f23da48f2dc0869f5a7f9747f58a0
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-02-28 14:04:43 -0300

    [Response] Bugfix.

--- Objects/response_object.py
+++ Objects/response_object.py
@@ -2,8 +2,14 @@ from datetime import datetime
 
 class Response():
     """ The object to be returned in the api. It contains the request status and the pdf document.
+        
+        Args:
+            pdf_document (list, optional): the bytes list of the downloaded document. Defaults to None.
+            status (bool, optional): true if the capture was correct. Defaults to False.
+            error_message (str, optional): if there's any error. Defaults to "".
     """
-    def __init__(self, pdf_document, status, error_message):
+    def __init__(self, pdf_document=None, status=False, error_message=""):
+
         self.pdf_document  = pdf_document
         self.status        = status
         self.error_message = error_message
@@ -15,7 +21,7 @@ class Response():
         Returns:
             dictionary: the response object serialized
         """
-        return { "PDF": self.pdfDocument,
+        return { "PDF": self.pdf_document,
                 "Status": self.status,
                 "ErrorMessage": self.error_message,
                 "CurrentDate": self.current_date}
