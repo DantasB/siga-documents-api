1d39d82520ec58019f9f2ac4ca7a6ba7a2b8bd72
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-03-25 00:25:50 -0300

    Changing main.py post parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from SharedLibrary.pdf_utils import *
 from SharedLibrary.siga_utils import *
 from Objects.response_object import *
@@ -13,22 +13,31 @@ loop = asyncio.get_event_loop()
 def get_disponible_document_type():
     return jsonify(list(list_of_documents.keys()))
 
-@app.route('/<login>/<password>/<document_type>')
-def get_document(login, password, document_type):
+@app.route('/document')
+def get_document():
+    login = request.args.get('login')
+    password = request.args.get('password')
+    document_type = request.args.get('doctype')
+
     response = Response()
 
-    if(treat_login(login) == ""):
+    if login is None or password is None or document_type is None:
+        response.error_message = "PLEASE SET YOUR PARAMETERS LIKE: url?login=something&password=8888&doctype=crid"
+        response.status        = False
+        return jsonify(response.serialize())
+
+    if treat_login(login) == "":
         response.error_message = "LOGIN IS NOT A VALID CPF. CHECK YOUR CREDENTIALS."
         response.status        = False
         return jsonify(response.serialize())
     
-    if(document_type not in list_of_documents):
+    if document_type not in list_of_documents:
         response.error_message = "DOCUMENT TYPE NOT VALID. CHECK YOUR CREDENTIALS."
         response.status        = False
         return jsonify(response.serialize())
     
     pdf_path, pdf = loop.run_until_complete(get_document_from_siga(login, password, document_type))
-    if(not is_valid_file_path(pdf_path)):
+    if not is_valid_file_path(pdf_path):
         response.error_message = "PDF NOT VALID. CHECK IF YOU HAVE THIS DOCUMENT, YOUR CREDENTIALS ARE CORRECT OR IF SIGA IS NOT ONLINE."
         response.status        = False
         return jsonify(response.serialize())
